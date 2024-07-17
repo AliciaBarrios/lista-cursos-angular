370381258c3f385b1ca1d8048421d6b2959b2cbf
Alicia Barrios Garcia <abarriosga@uoc.edu>
2024-07-17 20:17:00 +0200

    create mock files

---
+++ src/app/tabla-alumnos/mock.ts
@@ -0,0 +1,22 @@
+export const alumnos = [
+    {
+        nombre: 'Alicia',
+        apellido: 'Barrios',
+        promedio: 8.5
+    },
+    {
+        nombre: 'Sergio',
+        apellido: 'Giraldez',
+        promedio: 9.5
+    },
+    {
+        nombre: 'Miriam',
+        apellido: 'Huertas',
+        promedio: 9
+    },
+    {
+        nombre: 'Monica',
+        apellido: 'García',
+        promedio: 6.5
+    }
+]
---
+++ src/app/tabla-cursos/mock.ts
@@ -0,0 +1,22 @@
+export const cursos = [
+    {
+        materia: 'JavaScript',
+        dificultad: 9,
+        requiere: 'HTML y CSS'
+    },
+    {
+        materia: 'TypeScript',
+        dificultad: 7,
+        promedrequiereio: 'JavaScript'
+    },
+    {
+        materia: 'Angular',
+        dificultad: 9.5,
+        requiere: 'HTML, CSS, JS y TS'
+    },
+    {
+        materia: 'React',
+        dificultad: 7,
+        requiere: 'HTML, CSS, JS'
+    }
+]
